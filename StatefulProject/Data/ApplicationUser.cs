8c985be4f7f1608bbf435e4062fbef92f2ea07a6
LeTorky <24torky@gmail.com>
2022-05-09 23:00:54 +0200

    Editted ApplicationUser Columns

--- StatefulProject/Data/ApplicationUser.cs
+++ StatefulProject/Data/ApplicationUser.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using StatefulProject.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StatefulProject.Data
 {
     public class ApplicationUser:IdentityUser
     {
-        //Add Columns to Identity User.
+        [ForeignKey("Student")]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public virtual Student Student { get; set; }
     }
 }
